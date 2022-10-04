c45390e02c77e12f6273c6f0c4030b343cb196e4
akashkuchankar <kuchankarakash@gmail.com>
2022-10-04 06:22:36 -0700

    Add project files.

---
+++ print number 20 to 50 by for loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_number_20_to_50_by_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =20; i<=50; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
